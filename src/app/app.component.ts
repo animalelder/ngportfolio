ea9bd388d1318fde5bc01487e5df6b51e053ae2c
animalelder <wllearned@icloud.com>
2024-07-07 22:44:35 -0700

    Setup project: Angular, Tailwind, DaisyUI, SeamlessUI
    [Feature] Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserComponent } from './browser/browser.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, BrowserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ngportfolio';
